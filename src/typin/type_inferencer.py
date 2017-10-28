908da4e4af4534cc598bf996c857edd39b21a02d
Paul Ross <apaulross@gmail.com>
2017-10-28 13:05:52 +0100

    Progress on typeshed stubfile generation.

--- src/typin/type_inferencer.py
+++ src/typin/type_inferencer.py
@@ -5,29 +5,38 @@ Created on 22 Jun 2017
 """
 from collections import OrderedDict
 import inspect
-import re
+import logging
+import pprint
 import sys
 
 from typin import str_id_cache
+from typin import types
 
 # TODO: How to create the taxonomy? collections.abc [Python3]?
-
     
 class FunctionTypes(object):
+    TYPE_NAME_TRANSLATION = {
+        '_io.StringIO' : 'IO[bytes]',
+        'NoneType' : 'None',
+    }
     def __init__(self):
         # TODO: Track a range of line numbers.
         # 'call' must be always the same line number
         # Since functions can not overlap the 'return' shows function bounds
         #
-        # dict of {name : set(types), ...}
+        # OrderedDict of {argument_name : set(types.Type), ...}
         self.arguments = OrderedDict()
-        self.return_types = set()
-        self.exception_types = set()
+        # dict of {line_number : set(types.Type), ...}
+        self.return_types = {}
+        # dict of {line_number : set(types.Type), ...}
+        self.exception_types = {}
+        # Should be unique
+        self.call_line_number = 0
         # TODO: Track call/return type pairs so we can use the @overload
-        # decorator in th e.pyi files.
+        # decorator in the .pyi files.
         
-    def add_call(self, frame):
-        """Adds a function call from the frame"""
+    def add_call(self, frame, line_number):
+        """Adds a function call from the frame."""
         # arg_info is an ArgInfo object that has the following attributes:
         # args - list of names as strings.
         # varargs - name entry in the locals for *args or None.
@@ -35,31 +44,95 @@ class FunctionTypes(object):
         # locals - dict of {name : value, ...} of arguments.
         arg_info = inspect.getargvalues(frame)
         for arg in arg_info.args:
-            self.arguments[arg] = type(arg_info.locals[arg])
+            try:
+                self.arguments[arg].add(types.Type(arg_info.locals[arg]))
+            except KeyError:
+                self.arguments[arg] = set([types.Type(arg_info.locals[arg])])
+        if self.call_line_number == 0:
+            self.call_line_number = line_number
+        elif self.call_line_number != line_number:
+            raise ValueError('Call site was {:d} now {:d}'.format(
+                self.call_line_number, line_number)
+            )
     
-    def add_return(self, return_type):
-        self.return_types.add(return_type)
-
-    def add_exception(self, exception_type):
-        self.exception_types.add(exception_type)
+    def add_return(self, return_value, line_number):
+        t = types.Type(return_value)
+        try:
+            self.return_types[line_number].add(t)
+        except KeyError:
+            self.return_types[line_number] = set([t])
         
+    def add_exception(self, exception, line_number):
+        t = types.Type(exception)
+        try:
+            self.exception_types[line_number].add(t)
+        except KeyError:
+            self.exception_types[line_number] = set([t])
+    
     def __str__(self):
         """Returns something like the annotation string."""
         sl = ['type:']
-        for k in self.arguments:
-            sl.append('({:s})'.format(str(self.arguments[k])))
-        if len(self.return_types) == 0:
+        for arg in self.arguments:
+            arguments = sorted(self.arguments[arg])
+            if len(arguments) == 1:
+                sl.append('({:s} {:s})'.format(arg, str(arguments[0])))
+            else:
+                sl.append(
+                    '({:s} {!r:s})'.format(
+                        arg,
+                        ', '.join([str(v) for v in arguments])
+                    )
+                )                
+        # self.return_types is a dict of {line_number : set(types.Type), ...}
+        return_types = set()
+        for v in self.return_types.values():
+            return_types |= v
+        if len(return_types) == 0:
             sl.append('-> None')
-        elif len(self.return_types) == 1:
-            t = self.return_types.pop()
-            sl.append('-> {:s}'.format(str(t)))
-            # Preserve set
-            self.return_types.add(t)
+        elif len(return_types) == 1:
+            sl.append('-> {:s}'.format(str(return_types.pop())))
         else:
-            sl.append('-> Union[{:s}]'.format(', '.join((t for t in self.return_types))))
+            sl.append('-> Union[{:s}]'.format(', '.join((t for t in return_types))))
         return ' '.join(sl)
     
     __repr__ = __str__
+    
+    def _type(self, name):
+        """Translates a type name if necessary."""
+        return self.TYPE_NAME_TRANSLATION.get(name, name)
+        
+    def stub_file_str(self):
+        """Example: def encodebytes(s: bytes) -> bytes: ..."""
+        sl = ['(']
+        arg_str_list = []
+        for arg_name in self.arguments:
+            arguments = sorted(self.arguments[arg_name])
+            if len(arguments) == 1:
+                arg_str_list.append('{:s}: {:s}'.format(
+                    arg_name,
+                    self._type(str(arguments[0]))))
+            else:
+                arg_str_list.append(
+                    '{:s}: {:s}'.format(
+                        arg_name,
+                        ', '.join([self._type(str(v)) for v in arguments])
+                    )
+                )
+        sl.append(', '.join(arg_str_list)) 
+        # self.return_types is a dict of {line_number : set(types.Type), ...}
+        sl.append(') ->')
+        return_types = set()
+        for v in self.return_types.values():
+            return_types |= v
+        if len(return_types) == 0:
+            sl.append(' None')
+        elif len(return_types) == 1:
+            sl.append(' {:s}'.format(self._type(str(return_types.pop()))))
+        else:
+            sl.append(' Union[{:s}]'.format(', '.join((self._type(t) for t in return_types))))
+        sl.append(': ...')
+        return ''.join(sl)
+        
 
 class TypeInferencer(object):
     """Infers types of function arguments and return values at runtime."""
@@ -67,43 +140,81 @@ class TypeInferencer(object):
     def __init__(self):
         """Constructor."""
         # dict of {file_path : { function_name : FunctionTypes, ...}, ...} 
-        self._fn_map = {}
-        # Allow re-entrancy with sys.setprofile(profilefunc)
+        self.function_map = {}
+        # Allow re-entrancy with sys.settrace(function)
         self._trace_fn_stack = []
                 
     def _get_func_data(self, file_path, function_name):
-        if file_path not in self._fn_map:
-            self._fn_map[file_path] = {}
-        if function_name not in self._fn_map[file_path]:
-            self._fn_map[file_path][function_name] = FunctionTypes()
-        r = self._fn_map[file_path][function_name]
+        """Return a FunctionTypes() object for the function."""
+        if file_path not in self.function_map:
+            self.function_map[file_path] = {}
+        if function_name not in self.function_map[file_path]:
+            self.function_map[file_path][function_name] = FunctionTypes()
+        r = self.function_map[file_path][function_name]
         return r
     
     def __call__(self, frame, event, arg):
+        logging.debug('TypeInferencer.__call__', event, arg)
         if event in ('call', 'return', 'exception'):
             frame_info = inspect.getframeinfo(frame)
             file_path = frame_info.filename
+            # TODO: For methods use __qualname__
+            function_name = frame_info.function
             lineno = frame_info.lineno
             
+#             print()
+#             print(function_name)
+#             print(inspect.getframeinfo(frame))
 #             print(frame)
+
 #             print(dir(frame))
+#             print(' Frame '.center(75, '-'))
+#             for attr in dir(frame):
+#                 if attr.startswith('_') or attr in ('f_builtins', ):#'f_globals'):
+#                     continue
+#                 if attr == 'f_globals':
+#                     pprint.pprint(getattr(frame, attr))
+#                 else:
+#                     print(attr, ':', type(getattr(frame, attr)), getattr(frame, attr))
+#             print(' END: Frame '.center(75, '-'))
+
+#             print(' GC Frame '.center(75, '-'))
+#             import gc
+#             for o in gc.get_objects():
+#                 if inspect.isfunction(o):
+# #                     print(dir(o))
+# #                     print(o.__qualname__)
+#                     # See: https://stackoverflow.com/questions/1132543/getting-callable-object-from-the-frame
+#                     # Py2: if o.func_code is frame.f_code:
+#                     if o.__code__ is frame.f_code:
+#                         print(type(o), o, o.__qualname__) 
+#             print(' END: GC Frame '.center(75, '-'))
+# #             print(frame.f_code.co_name)
+# #             print(frame.f_globals[frame.f_code.co_name], frame.f_globals[frame.f_code.co_name].__qualname__)
+# 
 #             print(frame.f_code)
 #             print(dir(frame.f_code))
-#             print(frame.function.__qualname__)
+
+#             print(self.__exit__.__qualname__)
             
-            # TODO: use __qualname__
-            func_info = self._get_func_data(file_path,
-                                            frame_info.function)
+#             print(' Code '.center(75, '-'))
+#             for attr in dir(frame.f_code):
+#                 if not attr.startswith('_'):
+#                     print(attr, ':', type(getattr(frame.f_code, attr)), getattr(frame.f_code, attr))
+#             print(' END Code '.center(75, '-'))
+            
+#             print(frame.function.__qualname__)
+            func_types = self._get_func_data(file_path, function_name)
             if event == 'call':
                 # arg is None
-                func_info.add_call(frame)
+                func_types.add_call(frame, lineno)
             elif event == 'return':
                 # arg is return value
-                func_info.add_return(type(arg))
+                func_types.add_return(arg, lineno)
             else:
                 assert event == 'exception'
-                # arg is a tuple (exception, value, traceback)
-                pass
+                # arg is a tuple (exception_type, exception_value, traceback)
+                func_types.add_exception(arg[1], lineno)
         return self
     
     def __enter__(self):
--- tests/unit/test_type_inferencer.py
+++ tests/unit/test_type_inferencer.py
@@ -3,6 +3,8 @@ Created on 22 Jun 2017
 
 @author: paulross
 '''
+import io
+import base64
 import pprint
 import sys
 
@@ -15,14 +17,76 @@ def test_creation():
     assert t is not None
 
 
-def func_single_arg(arg):
-    # Do some stuff
+def func_single_arg_no_return(arg):
     pass
 
+def func_single_arg_return_arg(arg):
+    return arg
+
+def _pretty_print(ti):
+    # {file_path : { function_name : FunctionTypes, ...}
+    # Example of stub file:
+    # def encodebytes(s: bytes) -> bytes: ...
+    for file_path in sorted(ti.function_map):
+        print('File: {:s}'.format(file_path))
+        for function_name in sorted(ti.function_map[file_path]):
+            print('def {:s}{:s}'.format(
+                function_name,
+                ti.function_map[file_path][function_name].stub_file_str()
+                )
+            )
 
 def test_single_function():
     with type_inferencer.TypeInferencer() as ti:
-        func_single_arg('string')
-#     print()
-#     pprint.pprint(ti._fn_map)
+        func_single_arg_no_return('string')
+        func_single_arg_return_arg('string')
+    print()
+    print('test_single_function()')
+    _pretty_print(ti)
+#     pprint.pprint(ti.self.function_map)
 
+def func_that_raises():
+    raise ValueError('Error message')
+
+def test_single_function_that_raises():
+    with type_inferencer.TypeInferencer() as ti:
+        try:
+            func_that_raises()
+        except ValueError:
+            pass
+    print()
+    print('test_single_function_that_raises()')
+    _pretty_print(ti)
+#     pprint.pprint(ti.self.function_map)
+
+def test_typeshed_base64():
+    """From the typeshed: https://github.com/python/typeshed/blob/master/stdlib/2and3/base64.pyi
+    
+    def decode(input: IO[bytes], output: IO[bytes]) -> None: ...
+    def decodebytes(s: bytes) -> bytes: ...
+    def decodestring(s: bytes) -> bytes: ...
+    def encode(input: IO[bytes], output: IO[bytes]) -> None: ...
+    def encodebytes(s: bytes) -> bytes: ...
+    def encodestring(s: bytes) -> bytes: ...
+    """
+    with type_inferencer.TypeInferencer() as ti:
+        stream_in = io.StringIO()
+        stream_out = io.StringIO()
+        base64.encode(stream_in, stream_out)
+        base64.decode(stream_in, stream_out)
+        base64.encodebytes(b'')
+        base64.decodebytes(b'')
+        base64.decode(stream_in, stream_out)
+        encoded = base64.encodestring(b'')
+        decoded = base64.encodestring(encoded)
+    print()
+    print('test_typeshed_base64()')
+    _pretty_print(ti)
+    # def _input_type_check(s: 'bytes') -> NoneType: ...
+    # def decode(input: '_io.StringIO', output: '_io.StringIO') -> NoneType: ...
+    # def decodebytes(s: 'bytes') -> bytes: ...
+    # def encode(input: '_io.StringIO', output: '_io.StringIO') -> NoneType: ...
+    # def encodebytes(s: 'bytes') -> bytes: ...
+    # def encodestring(s: 'bytes') -> bytes: ...
+
+#     pprint.pprint(ti.function_map)
