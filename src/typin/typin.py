517d6988d25e147fff73bed5f5a99a293edd6694
Paul Ross <apaulross@gmail.com>
2017-11-06 13:31:17 +0000

    More tests. Start of command line interface.

--- src/typin/typin.py
+++ src/typin/typin.py
@@ -5,6 +5,15 @@ import logging
 import sys
 
 from typin import type_inferencer
+
+def compile_and_exec(filename, *args, **kwargs):
+    with open(filename) as f_obj:
+        src = f_obj.read()
+        code = compile(src, filename, 'exec')
+        with type_inferencer.TypeInferencer() as ti:
+            exec(code)
+        print(ti.pretty_format())
+
     
 def main():
     program_version = "v%s" % '0.1.1'
@@ -18,24 +27,24 @@ USAGE
 """ % (program_shortdesc, program_version)
     parser = argparse.ArgumentParser(description=program_license,
                             formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument("-c", action="store_true", dest="plot_conditional", default=False,
-                      help="Add conditionally included files to the plots. [default: %(default)s]")
-    parser.add_argument("-d", "--dump", action="append", dest="dump", default=[],
-                      help="""Dump output, additive. Can be:
-C - Conditional compilation graph.
-F - File names encountered and their count.
-I - Include graph.
-M - Macro environment.
-T - Token count.
-R - Macro dependencies as an input to DOT.
-[default: %(default)s]""")
-    parser.add_argument("-g", "--glob", action='append', default=[],
-            help="Pattern match to use when processing directories. [default: %(default)s] i.e. every file.")
-    parser.add_argument("--heap", action="store_true", dest="heap", default=False,
-                      help="Profile memory usage. [default: %(default)s]")
-    parser.add_argument("-k", "--keep-going", action="store_true",
-                         dest="keep_going", default=False,
-                         help="Keep going. [default: %(default)s]")
+#     parser.add_argument("-c", action="store_true", dest="plot_conditional", default=False,
+#                       help="Add conditionally included files to the plots. [default: %(default)s]")
+#     parser.add_argument("-d", "--dump", action="append", dest="dump", default=[],
+#                       help="""Dump output, additive. Can be:
+# C - Conditional compilation graph.
+# F - File names encountered and their count.
+# I - Include graph.
+# M - Macro environment.
+# T - Token count.
+# R - Macro dependencies as an input to DOT.
+# [default: %(default)s]""")
+#     parser.add_argument("-g", "--glob", action='append', default=[],
+#             help="Pattern match to use when processing directories. [default: %(default)s] i.e. every file.")
+#     parser.add_argument("--heap", action="store_true", dest="heap", default=False,
+#                       help="Profile memory usage. [default: %(default)s]")
+#     parser.add_argument("-k", "--keep-going", action="store_true",
+#                          dest="keep_going", default=False,
+#                          help="Keep going. [default: %(default)s]")
     parser.add_argument(
             "-l", "--loglevel",
             type=int,
@@ -44,32 +53,37 @@ R - Macro dependencies as an input to DOT.
             help="Log Level (debug=10, info=20, warning=30, error=40, critical=50)" \
             " [default: %(default)s]"
         )
-    parser.add_argument("-o", "--output",
+#     parser.add_argument("-o", "--output",
+#                          type=str,
+#                          dest="output",
+#                          default="out",
+#                          help="Output directory. [default: %(default)s]")
+#     parser.add_argument("-p", action="store_true", dest="ignore_pragma", default=False,
+#                       help="Ignore pragma statements. [default: %(default)s]")
+#     parser.add_argument("-r", "--recursive", action="store_true", dest="recursive",
+#                          default=False,
+#                       help="Recursively process directories. [default: %(default)s]")
+#     parser.add_argument("-t", "--dot", action="store_true", dest="include_dot",
+#                          default=False,
+#                       help="""Write an DOT include dependency table and execute DOT
+# on it to create a SVG file. [default: %(default)s]""")
+#     parser.add_argument("-G", action="store_true", dest="gcc_extensions",
+#                          default=False,
+#                       help="""Support GCC extensions. Currently only #include_next. [default: %(default)s]""")
+    parser.add_argument("-s", "--stubs",
                          type=str,
-                         dest="output",
-                         default="out",
-                         help="Output directory. [default: %(default)s]")
-    parser.add_argument("-p", action="store_true", dest="ignore_pragma", default=False,
-                      help="Ignore pragma statements. [default: %(default)s]")
-    parser.add_argument("-r", "--recursive", action="store_true", dest="recursive",
-                         default=False,
-                      help="Recursively process directories. [default: %(default)s]")
-    parser.add_argument("-t", "--dot", action="store_true", dest="include_dot",
-                         default=False,
-                      help="""Write an DOT include dependency table and execute DOT
-on it to create a SVG file. [default: %(default)s]""")
-    parser.add_argument("-G", action="store_true", dest="gcc_extensions",
-                         default=False,
-                      help="""Support GCC extensions. Currently only #include_next. [default: %(default)s]""")
-    parser.add_argument(dest="stubs", nargs=1, help="Path to write typeshed stubs.")
+                         dest="stubs",
+                         default="stubs",
+                         help="Directory to write stubs files. [default: %(default)s]")
+    parser.add_argument(dest="args", nargs='+', help="Arguments to execute.")
     args = parser.parse_args()
     logFormat = '%(asctime)s %(levelname)-8s %(message)s'
     logging.basicConfig(level=args.loglevel,
                         format=logFormat,
                         # datefmt='%y-%m-%d % %H:%M:%S',
                         stream=sys.stdout)
-    with type_inferencer.TypeInferencer() as ti:
-        pass
+    print(args)
+#     compile_and_exec(filename)#, *args, **kwargs)    
     return 0
 
 if __name__ == '__main__':
